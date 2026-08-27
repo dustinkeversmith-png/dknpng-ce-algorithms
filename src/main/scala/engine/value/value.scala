5c5b4c5b8346cef1bc2d9581ed4864201b894164
Dustin <dustinkeversmith@gmail.com>
2026-08-26 18:44:28 -0700

    update-mode

--- src/main/scala/engine/value/value.scala
+++ src/main/scala/engine/value/value.scala
@@ -4,40 +4,95 @@ import scala.collection.mutable.HashMap
 
 // So then I need a parser for the function which I want to be a string kind of or at least some kind of composable functional since the value types are accessible by string in the actual value these can obviously be expressed as intermediary stack, and base operations, assuming some BaseTypeOperator is always defined for the sub type.
 
+trait MemoryRef:
+  def valueType: ValueType
+
+  // When reading and writing it will get the offset and size of the value string from the dedicated type.
+
+  def read(value: String): Any
+  def write(value: String): Any
+
 trait FunctionalAst:
+
+    // Stored memory references to the arguments
+    def args: Map[String, MemoryRef]
+
+    // Possibly any extra intermediary values created or assigned while executing in a local scope
+    def stack: Map[String, Value]
+
+    
     // Then probably each line has its own Ast, it executes mean while mutating the stack
     // Then moving on, and then pushes that all back into the type maybe?
-    Map[string, Value] stack
+    
 
 trait Functional:
     Map[string, Value] stack
     Vector[string: args, operators: "", or like ast type thing but a program ast so it has access to stack or declaritive intermediary values that can be resampled]
 
 
+final class Functional(
+  val args: Vector[String],
+  val source: String
+):
 
-trait BaseTypeOperator:
+  // Parse/compile only once.
+  val program: FunctionalAst =
+    FunctionalCompiler.compile(source)
+  
+  // Then what about mixed modal operators so maybe it would be
+  // Map["Operator"]["ValueTypeA, ValueTypeB, ValueTypeC"]
+  // Where the ValueType is the t value of Value
+  // Then in execution, the arguments are already known to the value types, argument[]
+
+  def execute(
+    arguments: Map[String, Value]
+  ): Value =
+    program.execute(arguments)
+
+
+// Maybe we can just use a premade language with tree sitter some C like programming language like glsl or something.
+val add =
+  Functional(
+    args = Vector("value", "other"),
+    source =
+      """
+      result = value + other
+      """
+  )
+
+trait ValueOperators:
 
   // Map[String, Functional(Value a, Value b, ... However many values) -> Value] operator_set
 
   // Then can map independent operator overrides to the operator_set as functions.
+  def operatorSet: HashMap[String, Functional]
 
-  ["cast"] = 
-    Functional(
-        // So assuming we know the underlying name of a function say value, we then accept the line seperations, then it goes and executes this with a functional ast
-        "value += other_value * 2.0"
-        "intermediary ="
+ // Sets the operator under the name.
+  def register(
+    name: String,
+    functional: Functional
+  ): Unit =
+    operatorSet(name) = functional
 
-    )
+  // 
+  def operator(name: String): Functional =
+    operatorSet(name)
+
+  def execute(
+    name: String,
+    arguments: Map[String, Value]
+  ): Value =
+    operatorSet(name).execute(arguments)
+
+
+//   ["cast"] = 
+//     Functional(
+//         // So assuming we know the underlying name of a function say value, we then accept the line seperations, then it goes and executes this with a functional ast
+//         "value += other_value * 2.0"
+//         "intermediary ="
+
+//     )
     
-    (value: Any, targetType: String): Any
-
-  ["add"](a: Any, b: Any): Any
-
-  ["subtract"](a: Any, b: Any): Any
-
-  ["multiply"](a: Any, b: Any): Any
-
-  ["divide"](a: Any, b: Any): Any
 
 
 // Register of all base type sizes
